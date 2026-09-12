ab7c65ec867bc8221b2cc5dd962bea5ebb86da41
santoshml-lab <santoshkrsbg36@gmail.com>
2026-09-13 00:55:33 +0530

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>India Post Accessibility Audit</h1>
+      <p>Accessibility assessment platform</p>
+    </div>
+  );
+}
+
+export default App;
